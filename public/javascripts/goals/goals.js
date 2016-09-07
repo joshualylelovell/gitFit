41fbe156e4f75db675f4425248c601b70a80cccc
Benjamin West <benjaminwest1046@gmail.com>
2016-09-07 10:30:29 -0400

    Changed some styling to Materialize, made a Materialize navbar, changed goals.js to have mock cards

--- public/javascripts/goals/goals.js
+++ public/javascripts/goals/goals.js
@@ -2,6 +2,54 @@ angular.module('myApp')
 .component('goals', {
   template: `
     <h1>GOALS</h1>
+    <div class="center">
+    <div class="row">
+
+      <div class="card col l4">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="https://hd.unsplash.com/photo-1463334535327-f9009ae62bf7">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+          <p><a href="#">This is a link</a></p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+      </div>
+
+    <div class="card col l4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="https://hd.unsplash.com/photo-1463334535327-f9009ae62bf7">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+
+    <div class="card col l4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="https://hd.unsplash.com/photo-1463334535327-f9009ae62bf7">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+    </div>
+
+
+
 
     <div class="goals" ng-repeat = "goal in $ctrl.goals">
       <span ng-show="goal.completed" ng-click="$ctrl.toggle(goal)" class="glyphicon glyphicon-ok" aria-hidden="true"></span>
@@ -9,8 +57,7 @@ angular.module('myApp')
       <a ng-click="$ctrl.show(goal)">{{ goal.title }}</a>
       <button ng-click="$ctrl.delete(goal)" class="btn btn-xs btn-danger">X</button>
     </div>
-    <hr/>
-    <a ui-sref="goal-new" class="btn btn-primary">New</a>
+    <a ui-sref="goal-new" class="waves-effect waves-light btn">New</a>
   `,
   controller: function(goalService, $state) {
     this.goals = null;
