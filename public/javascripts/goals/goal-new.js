5323a4c0573e2027d04b2551fc96d9f0302b251b
Joshua Lovell <joshualylelovell@gmail.com>
2016-09-07 16:22:06 -0400

    updated new form with Materialize select field for fitness goal type

--- public/javascripts/goals/goal-new.js
+++ public/javascripts/goals/goal-new.js
@@ -1,59 +1,69 @@
 angular.module('myApp')
 .component('goalNew', {
   template: `
-    <h3>NEW</h3>
+    <h3>NEW FITNESS GOAL</h3>
 
     <form ng-submit="$ctrl.save()">
 
       <div class="form-group">
         <label for="title">What's your goal?</label>
-        <input type="text"
-               class="form-control"
-               name="title"
-               ng-model="$ctrl.goal.title">
+        <select class="form-control fitness-type"
+                name="title"
+                ng-model="$ctrl.goal.title">
+                <option value="" disabled selected>Choose your option</option>
+                <option value="Running">Running</option>
+                <option value="Weight Lifting">Weight Lifting</option>
+        </select>
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'running'">
+
+      <div class="form-group">
         <label for="dateToComplete">Completion Date</label>
         <input type="date"
                class="form-control"
                name="dateToComplete"
                ng-model="$ctrl.goal.dateToComplete">
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'running'">
+
+      <div class="form-group" ng-show="$ctrl.goal.title === 'Running'">
         <label for="distance">Distance (in miles)</label>
         <input type="number"
                class="form-control"
                name="distance"
                ng-model="$ctrl.goal.distance">
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'running'">
+
+      <div class="form-group" ng-show="$ctrl.goal.title === 'Running'">
         <label for="time">How long will it take you (in minutes)?</label>
         <input type="number"
                class="form-control"
                name="time"
                ng-model="$ctrl.goal.time">
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'weight lifting'">
+
+      <div class="form-group" ng-show="$ctrl.goal.title === 'Weight Lifting'">
         <label for="sets">How many sets can you do?</label>
         <input type="number"
                class="form-control"
                name="sets"
                ng-model="$ctrl.goal.sets">
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'weight lifting'">
+
+      <div class="form-group" ng-show="$ctrl.goal.title === 'Weight Lifting'">
         <label for="reps">How many reps can you do?</label>
         <input type="number"
                class="form-control"
                name="reps"
                ng-model="$ctrl.goal.reps">
       </div>
-      <div class="form-group" ng-show="$ctrl.goal.title === 'weight lifting'">
+
+      <div class="form-group" ng-show="$ctrl.goal.title === 'Weight Lifting'">
         <label for="muscleGroup">Which muscle group will you focus on?</label>
         <input type="text"
                class="form-control"
                name="muscleGroup"
                ng-model="$ctrl.goal.muscleGroup">
       </div>
+
       <div class="form-group">
         <label for="completed">Completed</label>
         <input type="checkbox"
