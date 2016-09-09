b52aa8b0400ed7228110ae298c03bcc69c8147ff
[mmurrah] <[mmurrah@airbridge.ac.nz]>
2016-09-09 14:26:19 -0400

    Parallax/home page styling

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,31 +1,59 @@
 angular.module('myApp')
 .component('home', {
   template: `
-  {{ $ctrl.parallax() }}
-
+<div class="outer-parallax">
    <div class="parallax-container">
     <div class="parallax"><img src="http://imgur.com/2lkgFB8.jpeg" alt="parallax1"></div>
   </div>
   <div class="section grey">
     <div class="row container">
-      <h2 class="header">Fitness</h2>
+      <h2 class="header">Set goals that fit your schedule</h2>
 
+      <b><p class="grey-text text-darken-3 lighten-3">
+      Our app, {{ $ctrl.name }}, uses unprecedented levels of technology to help you plan and track your fitness regime.
+      This app has been hand-forged using computers by developers who have amazing physiques.
+      <p></b>
 
-      <p class="grey-text text-darken-3 lighten-3">
+      <div class="cardbox">
+         <div class="row" class="hcard">
+          <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Card Title</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
 
-      FITSPO TEXT LOREM IPSUM
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      </p>
+        <div class="row" "hcard">
+          <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Card Title</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
 
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
   <div class="parallax-container">
     <div class="parallax"><img src="http://imgur.com/SXSnSQX.jpeg" alt="parallax2" id="pullup"></div>
   </div>
-
-  <h3> END OF HOME </h3>
-
+</div>
   `,
   controller: function() {
     this.name = 'Project 3 Fitness App';
