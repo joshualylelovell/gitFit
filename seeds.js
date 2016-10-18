d9ddfb614f667a3bd74e6392e54d6417b49b8d32
Joshua Lovell <joshualylelovell@gmail.com>
2016-10-18 09:59:01 -0400

    confirmed connection to MongoLab for seeds file when in production

--- seeds.js
+++ seeds.js
@@ -8,7 +8,20 @@ var Goal = require('./models/goal');
 // var User = require('./models/users');
 // var Goals = require('./models/goals');
 
-mongoose.connect('mongodb://localhost/project3-fitness');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/project3-fitness');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // our script will not exit until we have disconnected from the db.
 function quit() {
